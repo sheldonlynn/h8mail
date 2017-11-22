dbb671286e32b48e5cc39f29b01e7ef2848a07a1
Sheldon Lynn <sheldonlynn@outlook.com>
2017-11-22 14:00:36 -0800

    express + react

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([{
+    id: 1,
+    username: "user01"
+  }, {
+    id: 2,
+    username: "user02"
+  }]);
 });
 
 module.exports = router;
