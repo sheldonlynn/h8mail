f8acf3fec4441ffa8993f571609173e310dc008a
Sheldon Lynn <sheldonlynn@outlook.com>
2017-12-04 14:52:16 -0800

    Tim's script works

--- routes/email.js
+++ routes/email.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const faker = require('faker');
 const puppeteer = require('puppeteer');
+const _ = require('lodash');
 
 const postalCodes = [
   'L3Z 3G4',
@@ -31,37 +32,38 @@ const months = [
   "Dec"
 ];
 
-function getRandom(num) {
-  return Math.floor(Math.random() * (num + 1));
+async function getRandom(num) {
+  return Math.floor(Math.random() * num);
 }
 
 async function tims(page, email, firstName, lastName, postalCode, day, month, year) {
-  await page.evaluate((firstName, lastName, postalCode, email) => {
+  await page.evaluate((firstName, lastName, postalCode, email, day, month, year) => {
       document.querySelector('#email').value = email;
       document.querySelector('#firstname').value = firstName;
       document.querySelector('#lastname').value = lastName;
       document.querySelector('#postalcode').value = postalCode;
-      document.querySelector('#day31').value = "" + day;
-      document.querySelector('#month').value = "Mar";
-      document.querySelector('#year').value = "2006";
-    }, firstName, lastName, postalCode, email);
+      document.querySelector('#day31').value = day;
+      document.querySelector('#year').value = year;
+      document.querySelector('#month').value = month;
+  }, firstName, lastName, postalCode, email, day, month, year);
 
   await page.click('#consent');
 
-  // await page.evaluate(() => {
-  //   window.scrollBy(0, window.innerHeight / 2);
-  // });
-  //
-  // await page.click('.span12 > button.button-contact.submit');
+  await page.evaluate(() => {
+    window.scrollBy(0, window.innerHeight / 2);
+  });
+
+  await page.click('.span12 > button.button-contact.submit');
 }
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res) {
 
   (async() => {
 
     const browser = await puppeteer.launch({
-      headless: false,
+      /* headless: false,
+      slowMo : 250 */
     });
     const page = await browser.newPage();
     await page.goto('http://www.timhortons.com/ca/en/newsletter-signup.php');
@@ -69,15 +71,15 @@ router.get('/', function(req, res, next) {
 
     let firstName = faker.name.firstName();
     let lastName = faker.name.lastName();
-    let postalCode = postalCodes[getRandom(postalCodes.length)];
-    let email = faker.internet.email();
-    year = (getRandom(57) + 1960);
-    day = (getRandom(28) + 1);
-    month = months[getRandom(months.length)];
+    let postalCode = postalCodes[await getRandom(postalCodes.length)];
+    let email = req.body.email; //faker.internet.email();
+    let year = (await getRandom(57) + 1960);
+    let day = (await getRandom(28) + 1);
+    let month = months[await getRandom(months.length)];
 
     await tims(page, email, firstName, lastName, postalCode, day, month, year);
 
-    res.send('screenshot created');
+    res.send('subscribed');
   })();
 });
 
